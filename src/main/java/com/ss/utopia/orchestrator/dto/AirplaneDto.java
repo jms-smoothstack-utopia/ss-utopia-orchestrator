8c40542f99977ff13883cd3a9f22b724839ec651
Stephen Gerkin <stephen.gerkin@smoothstack.com>
2021-02-06 15:26:21 -0500

    Fix validations on DTO objects

--- src/main/java/com/ss/utopia/orchestrator/dto/AirplaneDto.java
+++ src/main/java/com/ss/utopia/orchestrator/dto/AirplaneDto.java
@@ -1,9 +1,11 @@
 package com.ss.utopia.orchestrator.dto;
 
+import javax.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class AirplaneDto {
 
+  @NotNull(message = "Max capacity required")
   private Integer maxCapacity;
 }
--- src/main/java/com/ss/utopia/orchestrator/dto/TicketsDto.java
+++ src/main/java/com/ss/utopia/orchestrator/dto/TicketsDto.java
@@ -1,21 +1,22 @@
 package com.ss.utopia.orchestrator.dto;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class TicketsDto {
 
-  @NotBlank(message = "Flight Number is required")
+  @NotNull(message = "Flight Number is required")
   private Long flightId;
 
-  @NotBlank(message = "Customer ID is required")
+  @NotNull(message = "Customer ID is required")
   private Long customerId;
 
   @NotBlank(message = "Passenger name is required")
   private String passenger;
 
-  @NotBlank(message = "Is the passenger check-in True or False?")
+  @NotNull(message = "Is the passenger check-in True or False?")
   private boolean isCheckedIn;
 
 }
