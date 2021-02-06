23dded9da3b040113d72ba3848060aa59efa6915
Stephen Gerkin <stephen.gerkin@smoothstack.com>
2021-02-06 12:17:37 -0500

    Move exception handling to cover all rest controllers

--- src/main/java/com/ss/utopia/orchestrator/controller/CustomerController.java
+++ src/main/java/com/ss/utopia/orchestrator/controller/CustomerController.java
@@ -3,34 +3,21 @@ package com.ss.utopia.orchestrator.controller;
 import com.ss.utopia.orchestrator.client.CustomerClient;
 import com.ss.utopia.orchestrator.dto.CustomerDto;
 import com.ss.utopia.orchestrator.model.Customer;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 import javax.validation.Valid;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
-
-  private static final Logger log = LoggerFactory.getLogger(Customer.class);
 
   private final CustomerClient client;
 
@@ -64,50 +51,5 @@ public class CustomerController {
   public ResponseEntity<?> delete(@PathVariable Long id) {
     client.deleteCustomer(id);
     return ResponseEntity.noContent().build();
-  }
-
-  @ExceptionHandler(value = HttpClientErrorException.class)
-  private ResponseEntity<?> httpClientErrorException(HttpClientErrorException ex) {
-    return ResponseEntity.status(ex.getStatusCode())
-        .headers(ex.getResponseHeaders())
-        .body(ex.getResponseBodyAsByteArray());
-  }
-
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
-  @ExceptionHandler(MethodArgumentNotValidException.class)
-  public Map<String, Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
-
-    Map<String, Object> response = new HashMap<>();
-    response.put("error", "Invalid field(s) in request.");
-    response.put("status", 400);
-
-    var errors = ex.getBindingResult()
-        .getAllErrors()
-        .stream()
-        .collect(
-            Collectors.toMap(error -> ((FieldError) error).getField(),
-                             error -> getErrorMessageOrDefault((FieldError) error)));
-
-    response.put("message", errors);
-    return response;
-  }
-
-  private String getErrorMessageOrDefault(FieldError error) {
-
-    var logMsg = "Validation exception - Message: '";
-
-    String errorMsg;
-    if (error.getDefaultMessage() == null) {
-      errorMsg = "Unknown validation failure";
-    } else {
-      errorMsg = error.getDefaultMessage();
-    }
-
-    logMsg += errorMsg + "' Field: " + error.getField()
-        + " Rejected Value: " + error.getRejectedValue();
-
-    log.debug(logMsg);
-
-    return errorMsg;
   }
 }
---
+++ src/main/java/com/ss/utopia/orchestrator/exception/ControllerAdvisor.java
@@ -0,0 +1,48 @@
+package com.ss.utopia.orchestrator.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+
+@RestControllerAdvice
+public class ControllerAdvisor {
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public Map<String, Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+    Map<String, Object> response = new HashMap<>();
+    response.put("error", "Invalid field(s) in request.");
+    response.put("status", 400);
+
+    var errors = ex.getBindingResult()
+        .getAllErrors()
+        .stream()
+        .collect(
+            Collectors.toMap(error -> ((FieldError) error).getField(),
+                             error -> getErrorMessageOrDefault((FieldError) error)));
+
+    response.put("message", errors);
+    return response;
+  }
+
+  private String getErrorMessageOrDefault(FieldError error) {
+    var msg = error.getDefaultMessage();
+    return msg == null ? "Unknown validation failure" : msg;
+  }
+
+  @ExceptionHandler(value = HttpClientErrorException.class)
+  private ResponseEntity<?> httpClientErrorException(HttpClientErrorException ex) {
+    return ResponseEntity.status(ex.getStatusCode())
+        .headers(ex.getResponseHeaders())
+        .body(ex.getResponseBodyAsByteArray());
+  }
+}
