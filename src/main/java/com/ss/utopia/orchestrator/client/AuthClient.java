6db6ccfcd5a70ef13341dcf5fcf3861a8d6f6b77
Stephen Gerkin <stephen.gerkin@smoothstack.com>
2021-02-18 11:44:07 -0500

    Add unit test for AuthClient

--- src/main/java/com/ss/utopia/orchestrator/client/AuthClient.java
+++ src/main/java/com/ss/utopia/orchestrator/client/AuthClient.java
@@ -2,8 +2,10 @@ package com.ss.utopia.orchestrator.client;
 
 import com.ss.utopia.orchestrator.dto.auth.CreateUserAccountDto;
 import java.util.UUID;
+import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +17,27 @@ import org.springframework.web.client.RestTemplate;
 public class AuthClient {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(AuthClient.class);
-  private final RestTemplate restTemplate;
   private final String endpoint = "/accounts/";
   private String apiHost;
+  private RestTemplateBuilder builder;
+  private RestTemplate restTemplate;
 
-  public AuthClient(RestTemplateBuilder restTemplateBuilder) {
-    this.restTemplate = restTemplateBuilder.build();
+  @Autowired
+  public void setBuilder(RestTemplateBuilder builder) {
+    this.builder = builder;
+  }
+
+  @PostConstruct
+  public void init() {
+    restTemplate = builder.build();
   }
 
   public void setApiHost(String apiHost) {
     this.apiHost = apiHost;
+  }
+
+  public String getEndpoint() {
+    return endpoint;
   }
 
   public ResponseEntity<UUID> createNewAccount(CreateUserAccountDto dto) {
---
+++ src/test/java/com/ss/utopia/orchestrator/client/AuthClientTest.java
@@ -0,0 +1,69 @@
+package com.ss.utopia.orchestrator.client;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.ss.utopia.orchestrator.dto.auth.CreateUserAccountDto;
+import java.net.URI;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+class AuthClientTest {
+
+  RestTemplateBuilder mockBuilder = Mockito.mock(RestTemplateBuilder.class);
+  RestTemplate mockRestTemplate = Mockito.mock(RestTemplate.class);
+  AuthClient clientToTest;
+  String mockApiHost = "/api/testing";
+
+  @BeforeEach
+  void beforeEach() {
+    when(mockBuilder.build())
+        .thenReturn(mockRestTemplate);
+    clientToTest = new AuthClient();
+    clientToTest.setBuilder(mockBuilder);
+    clientToTest.init();
+    verify(mockBuilder).build();
+
+    clientToTest.setApiHost(mockApiHost);
+  }
+
+  /**
+   * Very simple happy path test.
+   * <p>
+   * Validates the URL used for the rest template is composed of the injected apiHost and endpoint
+   * and that we receive the UUID returned by the rest template as the return result from the
+   * method.
+   */
+  @Test
+  void test_createNewAccount_ReturnsUUIDOfCreatedAccount() {
+    var uuid = UUID.randomUUID();
+    var createdUri = URI.create("/accounts/" + uuid);
+
+    var mockResponse = ResponseEntity
+        .created(createdUri)
+        .body(uuid);
+
+    var dto = CreateUserAccountDto.builder()
+        .email("test@test.com")
+        .password("abCD1234!@")
+        .build();
+
+    when(mockRestTemplate.postForEntity(mockApiHost + clientToTest.getEndpoint(),
+                                        dto,
+                                        UUID.class))
+        .thenReturn(mockResponse);
+
+    var result = clientToTest.createNewAccount(dto);
+
+    assertEquals(result.getBody(), uuid);
+    assertTrue(result.getHeaders().containsKey("Location"));
+    assertEquals(result.getHeaders().getLocation(), createdUri);
+  }
+}
